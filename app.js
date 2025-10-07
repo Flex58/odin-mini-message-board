7b4088842251c034bb09e6b6c13a380c4af05493
Flex58 <164533598+Flex58@users.noreply.github.com>
2025-10-07 12:16:36 +0200

    feat: add basic project structure and init express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("node:path");
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+
+const PORT = 3000;
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(PORT, (err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on Port ${PORT}`);
+});
